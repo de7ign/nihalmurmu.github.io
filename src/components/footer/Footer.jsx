f3a647154d3aa519a39918080d186b068355ab05
Nihal Murmu <nhlmrm@gmail.com>
2019-04-25 21:58:05 +0530

    Add social links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Paper, Typography, withStyles } from "@material-ui/core";
+import {
+  Paper,
+  Typography,
+  withStyles,
+  Grid,
+  Tooltip,
+  IconButton
+} from "@material-ui/core";
+import { Mail, InsertDriveFile } from "@material-ui/icons";
 
 const styles = theme => ({
   root: {
     backgroundColor: "#d8d8d8",
-    paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2,
+    paddingTop: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
     textAlign: "center"
   }
 });
@@ -14,10 +22,82 @@ const styles = theme => ({
 class Footer extends React.Component {
   render() {
     const { classes } = this.props;
+    const icons = [
+      [
+        <Tooltip title="mail">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="mailto:nhlmrm@gmail.com"
+          >
+            <IconButton>
+              <Mail />
+            </IconButton>
+          </a>
+        </Tooltip>
+      ],
+      [
+        <Tooltip title="twitter">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/nihalmurmu"
+          >
+            <IconButton>
+              <ion-icon name="logo-twitter" />
+            </IconButton>
+          </a>
+        </Tooltip>
+      ],
+      [
+        <Tooltip title="linkedin">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://linkedin.com/in/nihalmurmu"
+          >
+            <IconButton>
+              <ion-icon name="logo-linkedin" />
+            </IconButton>
+          </a>
+        </Tooltip>
+      ],
+      [
+        <Tooltip title="github">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/nihalmurmu"
+          >
+            <IconButton>
+              <ion-icon name="logo-github" />
+            </IconButton>
+          </a>
+        </Tooltip>
+      ],
+      [
+        <Tooltip title="resume">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://nihalmurmu.me/resume.pdf"
+          >
+            <IconButton>
+              <InsertDriveFile />
+            </IconButton>
+          </a>
+        </Tooltip>
+      ]
+    ];
     return (
       <div>
         <Paper className={classes.root} square>
           <Typography variant="overline">&copy; Nihal Murmu</Typography>
+          <Grid container justify="center" spacing={16}>
+            {icons.map(value => (
+              <Grid item>{value}</Grid>
+            ))}
+          </Grid>
         </Paper>
       </div>
     );
