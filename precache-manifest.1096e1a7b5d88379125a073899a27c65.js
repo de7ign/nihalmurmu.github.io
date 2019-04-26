e655d2b68fdf0baf366cab4da6514313e19fd9e2
Nihal Murmu <nhlmrm@gmail.com>
2019-04-27 02:40:14 +0530

    Updates

---
+++ precache-manifest.1096e1a7b5d88379125a073899a27c65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "511e472bfb00d27ff426e6467af9ced3",
+    "url": "/static/media/project-background.511e472b.jpeg"
+  },
+  {
+    "revision": "25d703bbde93435c43d2c4c4c63fa6f1",
+    "url": "/static/media/landing-background.25d703bb.jpeg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b896d98574eaa3925bcc",
+    "url": "/static/js/main.2f4c8a22.chunk.js"
+  },
+  {
+    "revision": "0d4abd386a112644a69a",
+    "url": "/static/js/2.f6c62bb1.chunk.js"
+  },
+  {
+    "revision": "b896d98574eaa3925bcc",
+    "url": "/static/css/main.bd5eb09c.chunk.css"
+  },
+  {
+    "revision": "53e8cd54bb791e48dff999f64391a3e7",
+    "url": "/index.html"
+  }
+];
